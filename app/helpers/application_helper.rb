0954fe1ec8a4bea04f7d25735e51b84459fda9ad
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-19 16:53:44 +0000

    implemented nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Mileni D.S.', 'All rights reserved'
   end
 end
